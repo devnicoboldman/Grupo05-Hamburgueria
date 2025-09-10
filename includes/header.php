95fb4355a9912d10f238caf7ae2570e38a237898
devnicoboldman <nicolas.bagatini@boldman.cc>
2025-09-10 16:01:30 -0300

    0.1
    Edição do header

--- includes/header.php
+++ includes/header.php
@@ -4,19 +4,31 @@
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Rock n' Burguer</title>
+  <!-- Fonte Outfit (Google Fonts) -->
+  <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <header>
-    <nav>
-      <a href="index.php">INÍCIO</a>
-      <a href="cardapio.php">CARDÁPIO</a>
-      <a href="carrinho.php">MEU CARRINHO</a>
+
+    <nav class="navbar">
+      <div class="logo">
+      <a href="index.php">
+        <img src="img/LOGOATUAL.png" alt="Rock n' Burguer" height="80">
+      </a>
+    </div>
+
+    <div class="menu">
+      <a href="index.php">Ínicio</a>
+      <a href="cardapio.php">Cardápio</a>
+      <a href="carrinho.php">Meu carrinho</a>
       <?php if (isset($_SESSION['usuario'])): ?>
         <span>Olá, <?= $_SESSION['usuario']['nome']; ?></span>
       <?php else: ?>
-        <a href="login.php">LOGIN</a>
+        <a href="login.php">Login</a>
       <?php endif; ?>
+    </div>
     </nav>
   </header>
