5ab1413fb8026c812be272dd9fa572aece7c5e46
devnicoboldman <nicolas.bagatini@boldman.cc>
2025-09-12 15:51:58 -0300

    teste

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,5 @@
 <?php
-// Ssssss formulário for enviado, processa os dados
+// Ssssssss formulário for enviado, processa os dados
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
